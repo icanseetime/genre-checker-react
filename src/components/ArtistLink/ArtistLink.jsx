0d7d5943d3b8c4bff0c992ee34ae602fb3de2e6f
icanseetime <54579944+icanseetime@users.noreply.github.com>
2021-11-06 18:31:52 +0100

    updated alt text to fit "no-image.png"

--- src/components/ArtistLink/ArtistLink.jsx
+++ src/components/ArtistLink/ArtistLink.jsx
@@ -8,7 +8,7 @@ export default function ArtistLink({ id, name, img }) {
 			<h2>{name}</h2>
 			<img
 				src={img || './no-image.png'}
-				alt={img ? name : 'Artist image missing, blank image'}
+				alt={img ? name : 'Circle with text inside saying no image available'}
 			/>
 		</Link>
 	)
